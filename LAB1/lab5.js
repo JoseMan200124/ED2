d4608ef182df0916b87bb6c06e8e1da0b06c238c
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2023-10-24 00:31:50 -0600

    Lab 5
    Adjunto laboratorio 5 completo.

--- LAB1/lab5.js
+++ LAB1/lab5.js
@@ -11,7 +11,7 @@ const ENCRYPTION_KEY = crypto.randomBytes(32).toString('hex');
 const IV_LENGTH = 16;
 
 class Person {
-    constructor(name, dpi, dateBirth, address, companies = {}, recruiter = '') {
+    constructor(name, dpi, dateBirth, address, companies = {}, recluiter) {
         this.name = name;
         this.dpi = dpi;
         this.dateBirth = dateBirth;
@@ -21,8 +21,9 @@ class Person {
         this.recommendations = [];
         this.recommendationsDecoded = [];
         this.conversations = [];
-        this.recruiter = recruiter;
-
+        this.recluiter = recluiter;
+        this.encryption = new AsymmetricEncryption();
+        this.uniqueKeyword = `${dpi}-estructuras`;
     }
     encryptConversation(conversation) {
         let iv = crypto.randomBytes(IV_LENGTH);
@@ -32,36 +33,7 @@ class Person {
         return iv.toString('hex') + ':' + encrypted.toString('hex');
     }
 
-    generateKeyPair() {
-        const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: {
-                type: 'spki',
-                format: 'pem'
-            },
-            privateKeyEncoding: {
-                type: 'pkcs8',
-                format: 'pem'
-            }
-        });
-        this.privateKey = privateKey;
-        this.publicKey = publicKey;
-    }
-    encryptWithPublicKey(data, publicKey) {
-        const encryptedData = crypto.publicEncrypt(
-            { key: publicKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
-            Buffer.from(data)
-        );
-        return encryptedData.toString('base64');
-    }
 
-    decryptWithPrivateKey(encryptedData) {
-        const decryptedData = crypto.privateDecrypt(
-            { key: this.privateKey, padding: crypto.constants.RSA_PKCS1_OAEP_PADDING },
-            Buffer.from(encryptedData, 'base64')
-        );
-        return decryptedData.toString();
-    }
     decryptConversation(encryptedData) {
         let parts = encryptedData.split(':');
         let iv = Buffer.from(parts.shift(), 'hex');
@@ -74,13 +46,24 @@ class Person {
         console.log(decrypted);
         return decrypted.toString();
     }
-
-    compress(data) {
-        return zlib.deflateSync(data).toString('base64');
+}
+class AsymmetricEncryption {
+    constructor() {
+        const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+        });
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
     }
 
-    decompress(compressedData) {
-        return zlib.inflateSync(Buffer.from(compressedData, 'base64')).toString();
+    encrypt(data) {
+        const encryptedData = crypto.publicEncrypt(this.publicKey, Buffer.from(data));
+        return encryptedData.toString('base64');
+    }
+
+    decrypt(encryptedData) {
+        const decryptedData = crypto.privateDecrypt(this.privateKey, Buffer.from(encryptedData, 'base64'));
+        return decryptedData.toString();
     }
 }
 
@@ -91,19 +74,7 @@ class Database {
         this.recommendationHuffman = new Huffman();
         this.companyHuffmans = {};
     }
-    testHuffman() {
-        const testText = "Tempora nihil dolor earum molestias. Deserunt nostrum amet doloremque qui et. Velit quia repellendus.\n" +
-            "Architecto optio maiores ipsum culpa. Quos optio incidunt omnis amet tenetur eum ut incidunt aut. Maiores atque omnis quis nulla odio.\n" +
-            "Enim commodi reiciendis qui et. Alias odit ipsa quibusdam cupiditate natus. Voluptate quibusdam possimus aut iusto magnam. Sint nobis adipisci dolores nobis est atque et expedita quibusdam.\n" +
-            "Totam et perferendis vitae explicabo nemo est vel. Aut earum distinctio quibusdam totam autem temporibus molestiae temporibus sed. Amet explicabo eveniet inventore quam. Earum soluta itaque natus.\n" +
-            "Optio porro nesciunt sint. Praesentium minus voluptatem sit. Quod dicta ea hic quis non. Officia mollitia ut inventore adipisci sit amet quas aut. Soluta sunt autem perspiciatis rerum porro et ut quia. Est sed voluptatem distinctio ut qui.";
-        const encoded = this.recommendationHuffman.encode(testText);
-        const decoded = this.recommendationHuffman.decode(encoded);
 
-        console.log("Original:", testText);
-        console.log("Codificado:", encoded);
-        console.log("Decodificado:", decoded);
-    }
     getCompanyHuffman(companyName) {
         if (!this.companyHuffmans[companyName]) {
             this.companyHuffmans[companyName] = new Huffman();
@@ -115,6 +86,10 @@ class Database {
     }
     decodeDPI(encodedDPI){
         return this.dpiHuffman.decode(encodedDPI);
+    }
+    validateIdentity(person) {
+        const encryptedWord = person.encryption.encrypt(person.uniqueKeyword);
+        return person.encryption.decrypt(encryptedWord) === person.uniqueKeyword;
     }
 
     search(k) {
@@ -255,8 +230,8 @@ const processCsvFile = (filePath, db, callback) => {
             }
 
             const data = JSON.parse(row.data);
-            const person = new Person(data.name, data.dpi, data.datebirth, data.address, data.companies, data.recruiter);
 
+            const person = new Person(data.name, data.dpi, data.datebirth, data.address, data.companies, data.recluiter);
             switch (row.operation) {
                 case 'INSERT':
                     db.insert(person);
@@ -289,7 +264,6 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 function showMenu() {
     console.log('\n===== Menú =====');
     console.log('1. Buscar por nombre');
@@ -297,9 +271,8 @@ function showMenu() {
     console.log('3. Mostrar cartas de recomendación por DPI');
     console.log('4. Mostrar conversaciones cifradas por DPI');
     console.log('5. Mostrar conversaciones descifradas por DPI');
-    console.log('6. Seleccionar reclutador y compañía y validar identidad');
+    console.log('6. Pruebas de llave pública y privada');
     console.log('7. Salir');
-
     rl.question('Elija una opción: ', (option) => {
         switch (option) {
             case '1':
@@ -339,40 +312,42 @@ function showMenu() {
                 });
                 break;
             case '6':
-                rl.question('Ingrese el reclutador: ', (reclutador) => {
-                    rl.question('Ingrese la compañía: ', (compania) => {
-                        validateIdentity(reclutador, compania, db);
+                rl.question('Ingrese el DPI de la persona: ', (dpi) => {
+                    const person = db.search(dpi);
+                    if (person) {
+                        rl.question('Ingrese el nombre del reclutador: ', (recruiterName) => {
+                            if (person.recluiter === recruiterName) {
+                                rl.question('Ingrese el nombre de la compañía: ', (companyName) => {
+                                    if (person.companies[companyName]) {
+                                        const isIdentityValid = db.validateIdentity(person);
+                                        if (isIdentityValid) {
+                                            console.log('Prueba exitosa: La palabra encriptada y luego desencriptada coincide con la original.');
+                                            showMenu();
+                                        } else {
+                                            console.log('Prueba fallida: La palabra desencriptada no coincide con la original.');
+                                            showMenu();
+                                        }
+                                    } else {
+                                        console.log('La compañía no se encuentra registrada para esta persona.');
+                                        showMenu();
+                                    }
+                                });
+                            } else {
+                                console.log('El nombre del reclutador no coincide con el registrado.');
+                                showMenu();
+                            }
+                        });
+                    } else {
+                        console.log('No se encontró a la persona con el DPI proporcionado.');
                         showMenu();
-                    });
+                    }
                 });
                 break;
-            case '7':
-                rl.close();
-                break;
-            default:
+                default:
                 showMenu();
                 break;
         }
     });
-}
-
-function validateIdentity(reclutador, compania, db) {
-    const message = 'palabra clave'; // Esto puede ser cualquier palabra clave que desees usar
-    let isValid = false;
-    db.data.forEach(person => {
-        if (person.recruiter === reclutador && Object.keys(person.companies).includes(compania)) {
-            const encryptedMessage = person.encryptWithPublicKey(message, person.publicKey);
-            const decryptedMessage = person.decryptWithPrivateKey(encryptedMessage);
-            if (decryptedMessage === message) {
-                isValid = true;
-            }
-        }
-    });
-    if (isValid) {
-        console.log('Identidad validada exitosamente.');
-    } else {
-        console.log('Identidad no pudo ser validada.');
-    }
 }
 function searchByEncodedDPI(){
     rl.question('\nIngrese el DPI codificado a buscar: ', (encodedDPI) =>{
@@ -426,7 +401,7 @@ function searchByName() {
 
 const db = new Database();
 
-processCsvFile('LAB1\\input_lab4.csv', db, () => {
+processCsvFile('LAB1\\input.csv', db, () => {
     db.data.forEach(person => {
         db.loadRecommendations(person);
         db.loadConversations(person);
